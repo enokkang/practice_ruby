82136132210edcc4a29a27b178728af0f3526322
enoch <enokkang@gmail.com>
2023-06-27 15:01:13 -0500

    added #ref

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,10 @@
+#Use the terminal to create a new folder in your Actualize folder called practice-ruby.
+#Create a new file in the folder called store_item.rb and open it in your text editor.
+#Use hashes with symbols to represent the following scenario:
+# You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+# Represent 3 items using hashes. Each hash should have the same keys with different values.
+# Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+
 item1 = { "color" => "red", "price" => 100, "name" => "T-Shirt" }
 item2 = { "color" => "blue", "price" => 200, "name" => "Ball" }
 item3 = { "color" => "green", "price" => 300, "name" => "Cup" }
