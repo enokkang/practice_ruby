429a0c3b832cf73ed14b62c5ae02c5c9b18e4fb6
enoch <enokkang@gmail.com>
2023-06-27 14:21:58 -0500

    added => and :

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,14 @@
-item1 = { :color => :red, :price => 100 }
-item2 = { :color => :blue, :price => 200 }
-item3 = { :color => :green, :price => 300 }
+item1 = { "color" => "red", "price" => 100 }
+item2 = { "color" => "blue", "price" => 200 }
+item3 = { "color" => "green", "price" => 300 }
+
+puts "#{item1["color"]} #{item1["price"]}"
+puts "#{item2["color"]} #{item2["price"]}"
+puts "#{item3["color"]} #{item3["price"]}"
+
+item1 = { :color => "red", :price => 100 }
+item2 = { :color => "blue", :price => 200 }
+item3 = { :color => "green", :price => 300 }
 
 puts "#{item1[:color]} #{item1[:price]}"
 puts "#{item2[:color]} #{item2[:price]}"
