479d7273585ef3d46746a4c5f52012382db6272c
enoch <enokkang@gmail.com>
2023-06-27 14:32:13 -0500

    added name

--- store_item.rb
+++ store_item.rb
@@ -1,15 +1,15 @@
-item1 = { "color" => "red", "price" => 100 }
-item2 = { "color" => "blue", "price" => 200 }
-item3 = { "color" => "green", "price" => 300 }
+item1 = { "color" => "red", "price" => 100, "name" => "T-Shirt" }
+item2 = { "color" => "blue", "price" => 200, "name" => "Ball" }
+item3 = { "color" => "green", "price" => 300, "name" => "Cup" }
 
-puts "#{item1["color"]} #{item1["price"]}"
-puts "#{item2["color"]} #{item2["price"]}"
-puts "#{item3["color"]} #{item3["price"]}"
+puts "#{item1["color"]} #{item1["price"]} #{item1["name"]}"
+puts "#{item2["color"]} #{item2["price"]} #{item2["name"]}"
+puts "#{item3["color"]} #{item3["price"]} #{item3["name"]}"
 
-item1 = { :color => "red", :price => 100 }
-item2 = { :color => "blue", :price => 200 }
-item3 = { :color => "green", :price => 300 }
+item1 = { :color => "red", :price => 100, :name => "T-Shirt" }
+item2 = { :color => "blue", :price => 200, :name => "Ball" }
+item3 = { :color => "green", :price => 300, :name => "Cup" }
 
-puts "#{item1[:color]} #{item1[:price]}"
-puts "#{item2[:color]} #{item2[:price]}"
-puts "#{item3[:color]} #{item3[:price]}"
+puts "#{item1[:color]} #{item1[:price]} #{item1[:name]}"
+puts "#{item2[:color]} #{item2[:price]} #{item2[:name]}"
+puts "#{item3[:color]} #{item3[:price]} #{item3[:name]}"
