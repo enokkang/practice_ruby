2e6dcbc6dce582f0e44e1f83f96a46ca025be0c4
enoch <enokkang@gmail.com>
2023-06-28 14:11:11 -0500

    added inheritance for Food

--- store_item.rb
+++ store_item.rb
@@ -64,6 +64,13 @@ class Item
   end
 end
 
+class Food < Item
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
 #item4 = Item.new("Red", 100, "T-Shirt")
 
 item4 = Item.new(color: "Red", price: 100, name: "T-Shirt")
@@ -72,3 +79,6 @@ item4.print_info
 #item4.set_price(400)
 item4.price = 500
 item4.print_info
+
+item5 = Food.new(color: "Yellow", price: 3, name: "Banana", shelf_life: 10)
+p item5
